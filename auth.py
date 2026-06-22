125692f303f0e1fdece8ca4781cfd5ef5e2eb87d
rafael <rafaelvillalobos_20@hotmail.com>
2026-06-21 20:50:10 -0400

    validations and comments

--- auth.py
+++ auth.py
@@ -15,23 +15,23 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 if not SECRET_KEY:
     raise ValueError("SECRET_KEY no configurada en .env")
 
-ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", "30"))
+ALGORITHM = "HS256" # algoritmo de encriptación
+ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", "30")) # expiración del token
 
-pwd_hash = PasswordHash.recommended()
+pwd_hash = PasswordHash.recommended() # algoritmo de hashing
 
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/auth/login")
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/auth/login") # url para el login
 
 
-def hash_password(plain: str) -> str:
+def hash_password(plain: str) -> str: # hashing de contraseñas
     return pwd_hash.hash(plain)
 
 
-def verify_password(plain: str, hashed: str) -> bool:
+def verify_password(plain: str, hashed: str) -> bool: # verificación de contraseñas
     return pwd_hash.verify(plain, hashed)
 
 
-def create_access_token(data: dict, expires_delta: timedelta | None = None) -> str:
+def create_access_token(data: dict, expires_delta: timedelta | None = None) -> str: # creación del token
     to_encode = data.copy()
     expire = datetime.now(timezone.utc) + (
         expires_delta or timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
@@ -40,11 +40,11 @@ def create_access_token(data: dict, expires_delta: timedelta | None = None) -> s
     return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
 
 
-def get_current_user(
+def get_current_user( # obtención del usuario actual
     token: str = Depends(oauth2_scheme),
     db: Session = Depends(get_db),
 ) -> User:
-    try:
+    try:# checkeo del token para verificación
         payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
         user_id: str | None = payload.get("sub")
         if user_id is None:
@@ -52,14 +52,14 @@ def get_current_user(
                 status_code=status.HTTP_401_UNAUTHORIZED,
                 detail="Token invalido: falta subject",
             )
-    except InvalidTokenError:
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
+    except InvalidTokenError: # si el token es invalido o expirado, se lanza una excepción
+        raise HTTPException(# se lanza una excepción con un código de estado 401 y un mensaje
+            status_code=status.HTTP_401_UNAUTHORIZED,#error 401 = unauthorized (no autorizado)
             detail="Token invalido o expirado",
         )
 
-    user = db.query(User).filter(User.id == user_id).first()
-    if user is None:
+    user = db.query(User).filter(User.id == user_id).first() # se busca el usuario en la base de datos con el id del token
+    if user is None: 
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
             detail="Usuario no encontrado",
--- database.py
+++ database.py
@@ -7,21 +7,26 @@ load_dotenv() # cargar variables del .env
 
 DATABASE_URL = os.getenv("DATABASE_URL") # tomando el string de la base de datos
 
+ssla ={} # variables para ssl
+capath="ca.pem" # ruta del certificado ssl
+if os.path.exists(capath):
+    ssla["ssl"] = {"ca":capath}
+
 if not DATABASE_URL:    #validacion
     raise ValueError("DATABASE_URL .env no tiene una valor.")
 
-engine = create_engine(
+engine = create_engine( # creando el engine para la base de datos 
     DATABASE_URL,
     echo=True,#ver query in la terminal
+    connect_args=ssla
 )
 
 sessionLocal = sessionmaker(autocommit = False, autoflush=False, bind=engine)
 
-class Base(DeclarativeBase):
+class Base(DeclarativeBase):# clase base para las tablas de la base de datos
     pass
 
-
-def get_db():
+def get_db(): # funcion para obtener el sesion local
     db= sessionLocal()
     try:
         yield db
